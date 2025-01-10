2eb6c70b1d0fc27f2bfa4f0b402aeb2b8abb1acf
wwrrtt <40827788+wwrrtt@users.noreply.github.com>
2025-01-11 00:46:28 +0800

    Update main.js

--- server/main.js
+++ server/main.js
@@ -1,15 +1,4 @@
 import { Meteor } from 'meteor/meteor';
-import { WebApp } from 'meteor/webapp';
-
-// 设置根路由返回 Hello World
-WebApp.connectHandlers.use('/', (req, res, next) => {
-  if (req.url === '/') {
-    res.writeHead(200, { 'Content-Type': 'text/html' });
-    res.end('<h1>Hello World</h1>');
-  } else {
-    next();
-  }
-});
 
 Meteor.startup(() => {
   console.log('Server is running...');
