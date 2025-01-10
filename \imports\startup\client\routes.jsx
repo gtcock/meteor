ed9a8b76d6f605c89ddd7d42b3c800277ecb15ff
wwrrtt <40827788+wwrrtt@users.noreply.github.com>
2025-01-10 14:35:21 +0900

    Delete \imports\startup\client\routes.jsx

--- \imports\startup\client\routes.jsx
+++
@@ -1,9 +0,0 @@
-import { FlowRouter } from 'meteor/kadira:flow-router';
-import { BlazeLayout } from 'meteor/kadira:blaze-layout';
-
-FlowRouter.route('/', {
-  name: 'home',
-  action() {
-    BlazeLayout.render('App_body', { main: 'Home' });
-  }
-}); 
